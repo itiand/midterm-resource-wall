afb25b5016156f1ff06d1b8042f845b7601d4a7e
caro-turnbull <caro.turnbull@gmail.com>
2023-07-02 15:49:37 -0400

    sign up form connected to database

--- routes/sign-up-route.js
+++ routes/sign-up-route.js
@@ -11,19 +11,40 @@ const db = require('../db/connection');
 
 router.get('/', (req, res) => {
 
-  // res.send("Hello!")
-  // db.query('SELECT * FROM users')
-  //                               .then(data => console.log('WALDO', data))
-  //                               .catch(err => console.log(err));
-  // return res.render('signup');
-
   db.query('SELECT * FROM users').then(data => {
-    const templateVars = {users: data.rows}
+    const templateVars = { users: data.rows };
     console.log('WALDO', templateVars);
     return res.render('signup', templateVars);
   })
-  .catch(err => console.log("dbQueryErr", err));
+    .catch(err => console.log("dbQueryErr", err));
 
 });
+
+router.post('/', (req, res) => {
+  console.log("waldo", req.body);
+  // db.query(`INSERT INTO users (name, username, email) VALUES (${req.body.name}, ${req.body.username}, ${req.body.email}) RETURNING *;`)
+  //   .then(data => {
+  //   console.log('WALI', data.rows);
+  //   // console.log('', data.rows)
+  //   // const templateVars = {users: data.rows}
+  //   // console.log('WALDO', templateVars);
+  //   return res.render('index');
+  // })
+  // .catch(err => console.log("dbQueryErr", err));
+
+
+  const { name, username, email } = req.body;
+  db.query('INSERT INTO users (name, username, email) VALUES ($1, $2, $3) RETURNING *;', [name, username, email])
+    .then(data => {
+      console.log('WALI', data.rows);
+      const templateVars = { users: data.rows };
+      return res.render('homepagethatdoesnotexistyet', templateVars);
+    })
+    .catch(err => console.log("dbQueryErr", err));
+
+
+});
+// INSERT INTO users (name, username, email) VALUES ('Chris', 'Del', 'chris@gmail.com');
+
 
 module.exports = router;
