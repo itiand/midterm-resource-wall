b2b2986863307bd7376019e05d649943d8b8ee16
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-02 18:48:32 -0400

    start home page, create route for /home

---
+++ routes/home-route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  // res.send("Hello!")
+  res.render('home');
+});
+
+module.exports = router;
+
