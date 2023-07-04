87bad4bf69d9992af0205622ec2332bd420017e1
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-04 10:07:57 -0400

    fix sql query and adjust seeds for resources

--- routes/home-route.js
+++ routes/home-route.js
@@ -4,19 +4,21 @@ const db = require('../db/connection');
 
 router.get('/', (req, res) => {
 
-  db.query(`SELECT resources.*, users.username, categories.name AS category_name, ROUND(AVG(number_rating), 1) as rating
+  db.query(`SELECT resources.*, users.username, categories.name AS category_name,
+  (SELECT ROUND(AVG(number_rating), 1)
+  FROM ratings
+  WHERE resources.id = ratings.resource_id) AS rating
   FROM resources
   JOIN users ON users.id = resources.user_id
-  JOIN categories ON categories.id = resources.category_id
-  JOIN ratings ON resources.id = ratings.resource_id
-  GROUP BY resources.id, resources.*, users.username, categories.name;
-  `)
-  .then(data => {
-    const templateVars = { resources: data.rows };
-    console.log('WALDO', templateVars);
+  JOIN categories ON categories.id = resources.category_id;
 
-    return res.render('home', templateVars);
-  })
+  `)
+    .then(data => {
+      const templateVars = { resources: data.rows };
+      console.log('WALDO', templateVars);
+
+      return res.render('home', templateVars);
+    })
     .catch(err => console.log("dbQueryErr", err));
 
 });
