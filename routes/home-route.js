6dc47c6ab6f67a176f1665578ee982b712f45f5d
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-04 12:26:37 -0400

    ajax request and filter for HTML route, the updated UI

--- routes/home-route.js
+++ routes/home-route.js
@@ -23,5 +23,32 @@ router.get('/', (req, res) => {
 
 });
 
+
+// Server route to handle the filtering
+router.get('/HTML', (req, res) => {
+  const filteredQuery = `SELECT resources.*, users.username, categories.name AS category_name,
+    (SELECT ROUND(AVG(number_rating), 1)
+    FROM ratings
+    WHERE resources.id = ratings.resource_id) AS rating
+    FROM resources
+    JOIN users ON users.id = resources.user_id
+    JOIN categories ON categories.id = resources.category_id
+    WHERE categories.name = 'HTML';`; // Replace 'html' with the selected category
+
+  db.query(filteredQuery)
+    .then(data => {
+      console.log('WE HERE', data.rows);
+
+      const filteredResources = data.rows;
+
+      return res.json({ resources: filteredResources });
+
+    })
+    .catch(err => {
+      console.log("dbQueryErr", err);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
 module.exports = router;
 
