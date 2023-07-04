a31d040fcd10f067e567b7c9056de6cab8352270
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-07-04 09:49:07 -0400

    Added functionality to the resource route in resource.js to retrieve a specific resource based on its ID. The route now accepts a dynamic parameter :id in the URL, allowing the rendering of the resource page.

--- routes/resource.js
+++ routes/resource.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/connection');
 
-router.get('/', (req, res) => {
 
-  res.render('resourcepage')
+router.get('/:id', (req, res) => {
+    let id = req.params.id
+    db.query('SELECT * FROM resources WHERE id = $1',[id]).then(data => {
+        const templateVars = { resource: data.rows[0]};
+        console.log('WALDO RESOURCE', templateVars);
+        return res.render('resourcepage', templateVars);
+      })
+        .catch(err => console.log("dbQueryErr", err));
 });
 
 module.exports = router;
