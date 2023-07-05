497730c1066af25e6aa0a2369d89833838570d1d
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-05 15:27:57 -0400

    login functionality set up, simple user fetch, password authentication, and redirect to home page

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,24 +1,39 @@
-/*
- * All routes for login are defined here
- * Since this file is loaded in server.js into /login,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
 
+const { findUserByEmail, isLoggedIn, isUsersURL } = require('../lib/helpers');
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 
 router.get('/', (req, res) => {
   // res.send("Hello!")
- res.render('login');
+  res.render('login');
 });
 
 
 router.post('/', (req, res) => {
+  const { email, password } = req.body;
 
-  console.log('TRUE', req.body); //{ username: 'chris', email: 'chris@gmail.com' }
-})
+  // Use the findUserByEmail function to find the user by email
+  findUserByEmail(email)
+    .then(user => {
+      if (user) {
+        console.log('userData', user);
+
+        //if found check password
+        if (user.password === password) {
+          res.redirect('/home');
+        } else {
+          console.log('wrong password');
+        }
+      } else {
+        //slide down --> user not found
+      }
+    })
+    .catch(err => {
+      res.status(500).send('Internal Server Error');
+    });
+
+});
 
 module.exports = router;
 
