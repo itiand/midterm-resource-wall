f0980d6fab7fd3d3de80c444af2c1b5ff18c4e54
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-07-06 16:06:41 -0400

    Move queries from /routes/resource.js to the queries folder

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -10,4 +10,23 @@ const searchResources = (text) => {
     });
 };
 
-module.exports = { searchResources };
+const getCommentsByResourceID = (resourceID) => {
+  return db.query('Select comments.*, users.username AS username FROM comments JOIN users ON users.id = comments.user_id where resource_id = $1;',[resourceID])
+  .then(data => { 
+    return data.rows
+  }).catch((err) => {
+    console.error(err)
+  })
+}
+
+const getResourceById = (id) => {
+  return db.query('SELECT resources.*,users.username AS username FROM resources JOIN users ON users.id = resources.user_id WHERE resources.id = $1', [id])
+  .then((data) => {
+    return data.rows[0]
+  }).catch((err) => {
+    console.error(err)
+  })
+}
+
+
+module.exports = { searchResources, getCommentsByResourceID, getResourceById };
