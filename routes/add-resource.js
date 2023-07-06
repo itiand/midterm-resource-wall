1562e6f0baa007a36706e87ac364b9cdf456bff6
caro-turnbull <caro.turnbull@gmail.com>
2023-07-06 17:27:42 -0400

    hardly any change

--- routes/add-resource.js
+++ routes/add-resource.js
@@ -11,9 +11,9 @@ const addResourceQueries = require('../db/queries/add');
 
 router.post('/', (req, res) => {
   console.log("rquest", req.body);
-  const user_id = 1;
+  ///const user_id = 1;
   addResourceQueries
-    .addResource(req.body.title, req.body.url, req.body.description, parseInt(req.body.category_id), user_id, req.body.photo_url)
+    .addResource(req.body.title, req.body.url, req.body.description, parseInt(req.body.category_id), req.session.user_id, req.body.photo_url)
     .then((data) => {
       const id = data[0].id
       console.log("id now", id, data)
