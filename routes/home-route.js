d98c375c60da6acfc92170b6a516fe94913b8e39
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-06 13:29:37 -0400

    add badgeDisplay in tempalteVars, like button on navbar now redirects to /home/liked

--- routes/home-route.js
+++ routes/home-route.js
@@ -13,8 +13,31 @@ router.get('/', (req, res) => {
   JOIN categories ON categories.id = resources.category_id;
   `)
     .then(data => {
-      const templateVars = { resources: data.rows };
+      const templateVars = { badgeDisplay: "d-none", resources: data.rows };
 
+      return res.render('home', templateVars);
+    })
+    .catch(err => console.log("dbQueryErr", err));
+});
+
+
+router.get('/liked', (req, res) => {
+  const userId = req.session.user_id;
+  const filteredQuery = `
+    SELECT resources.*, users.username, categories.name AS category_name,
+      (SELECT ROUND(AVG(number_rating), 1)
+      FROM ratings
+      WHERE resources.id = ratings.resource_id) AS rating
+    FROM resources
+    JOIN users ON users.id = resources.user_id
+    JOIN categories ON categories.id = resources.category_id
+    JOIN favourites ON favourites.resource_id = resources.id
+    WHERE favourites.user_id = $1;
+  `;
+  db.query(filteredQuery, [userId])
+    .then(data => {
+      const templateVars = { badgeDisplay: "d-inline", resources: data.rows };
+      console.log('LIKED', templateVars);
       return res.render('home', templateVars);
     })
     .catch(err => console.log("dbQueryErr", err));
@@ -23,7 +46,6 @@ router.get('/', (req, res) => {
 
 router.get('/category/:category', (req, res) => {
   const category = req.params.category;
-
   const filteredQuery = `SELECT resources.*, users.username, categories.name AS category_name,
     (SELECT ROUND(AVG(number_rating), 1)
     FROM ratings
