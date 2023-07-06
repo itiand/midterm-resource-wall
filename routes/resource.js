0266b0a0ee367ccc01f23f48a4b10ed40080f543
caro-turnbull <caro.turnbull@gmail.com>
2023-07-06 16:09:04 -0400

    nav bar glitches and inconsistencies

--- routes/resource.js
+++ routes/resource.js
@@ -4,7 +4,8 @@ const db = require('../db/connection');
 
 
 router.get('/:id', (req, res) => {
-    let id = req.params.id
+    let id = parseInt(req.params.id)
+    console.log("ids", id, req.params.id)
     db.query('SELECT resources.*,users.username AS username FROM resources JOIN users ON users.id = resources.user_id WHERE resources.id = $1',[id]).then(data => {
         const templateVars = { resource: data.rows[0]};
         console.log('WALDO RESOURCE', templateVars);
@@ -15,7 +16,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/:id/like', (req, res) => {
     let id = req.params.id
-    // const userid = req.session.userid 
+    // const userid = req.session.userid
     const user_id = 1
     db.query('INSERT INTO favourites (resource_id, user_id) VALUES ($1, $2);',[id, user_id]).then(data => {
         res.send({message:"Resource Liked"})
@@ -24,7 +25,7 @@ router.post('/:id/like', (req, res) => {
 });
 router.get('/:id/comment', (req, res) => {
     let id = req.params.id
-    // const userid = req.session.userid 
+    // const userid = req.session.userid
     const user_id = 1
     db.query('Select * from comments where resource_id = $1 and  user_id = $2;',[id, user_id]).then(data => {
         const templateVars = { comments: data.rows[0]};
@@ -37,11 +38,11 @@ router.post('/:id/comment', (req, res) => {
     let id = req.params.id
     let comment = req.body.comment
     console.log(comment, req,req.body)
-    // const userid = req.session.userid 
+    // const userid = req.session.userid
     const user_id = 1
     db.query('INSERT INTO comments (resource_id, user_id, comment) VALUES ($1, $2, $3);',[id, user_id,comment]).then(data => {
         res.send({message:"Resource Comment"})
-       
+
       })
         .catch(err => console.log("dbQueryErr", err));
 });
