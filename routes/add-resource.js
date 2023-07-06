60850de3e216ea9b4a3f57a2f3165b144c7a380d
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-06 16:14:11 -0400

    quick clean up

--- routes/add-resource.js
+++ routes/add-resource.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const addResourceQueries = require('../db/queries/add');
 
 
 
 router.get('/', (req, res) => {
   // res.send("Hello!")
- res.render('add-resource');
+  res.render('add-resource');
+
 });
 
 router.post('/', (req, res) => {
-  console.log("rquest", req.body)
-  const user_id = 1
+  console.log("rquest", req.body);
+  const user_id = 1;
   addResourceQueries
     .addResource(req.body.title, req.body.url, req.body.description, req.body.category_id, user_id, req.body.photo_url)
     .then(() => {
-      res.redirect("/resource/1") //to resource/:id ????
+      res.redirect("/resource/1"); //to resource/:id ????
     })
     .catch((e) => {
       console.error(e);
